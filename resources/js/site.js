af6a9d671f6ee45bca48e4b810c85bae365eb7f4
Afan417 <batman.two@hotmail.com>
2025-02-11 17:25:46 +0100

    Cleanup

--- resources/js/site.js
+++
@@ -1,9 +0,0 @@
-import "@fontsource/poppins/latin-ext.css";
-import { Demo } from "./alpine/Demo";
-import Alpine from "alpinejs";
-
-window.Alpine = Alpine;
-
-Alpine.data("demo", Demo);
-
-Alpine.start();
